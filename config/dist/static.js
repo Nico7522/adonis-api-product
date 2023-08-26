c8fcca1094befa953f32c285d1944e5fd13407f6
Nicolas <nico.daddabbo7100@gmail.com>
2023-08-26 12:30:23 +0200

    ajout dossier public et config pour image

---
+++ config/dist/static.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var staticConfig = {
+    enabled: true,
+    dotFiles: 'ignore',
+    etag: true,
+    lastModified: true
+};
+exports["default"] = staticConfig;
